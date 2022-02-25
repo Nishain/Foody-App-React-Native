7264ca2e12542dca46fceec8816cf1ef719cf9c1
Nishain <nishain.atomic@gmail.com>
2022-02-26 00:49:05 +0530

    added optional feature: food images to food,create,update,delete

--- components/FoodsScreen.js
+++ components/FoodsScreen.js
@@ -1,6 +1,6 @@
-import React, { useContext, useEffect, useMemo } from "react"
+import React, { useContext, useEffect, useMemo, useRef } from "react"
 import { useState } from "react"
-import { Alert, FlatList, StyleSheet, Text, View } from "react-native"
+import { Alert, FlatList, Image, StyleSheet, Text, View } from "react-native"
 import CustomButton from "./common/CustomButton"
 import CustomCard from "./common/CustomCard"
 import KeyValueText from "./common/KeyValueText"
@@ -11,97 +11,138 @@ import CartContext from "./contexts/CartContext"
 import Icon from 'react-native-vector-icons/FontAwesome'
 import CustomSnackBar from "./common/CustomSnackBar"
 import UserRoleContext from "./contexts/UserRoleContext"
+import storage from '@react-native-firebase/storage';
 
-export default function FoodBrowseScreen({navigation}) {
+export default function FoodBrowseScreen({ navigation }) {
     const reference = database().ref('/food/')
-    const [searchCriteria,setSearchCriteria] = useState(undefined)
+    const [searchCriteria, setSearchCriteria] = useState(undefined)
     const [data, setData] = useState([])
+    //need to maintain persistant reference pointer to 'data' state variable so
+    //that even old frozen function decalared in previous render can access fresh data in the
+    //state variable in current render therefore dataGlobalReference is defined....
+    const dataGlobalReference = useRef()
+    //need to update dataGlobalReference in each render with fresh data elimate old data in previous render...
+    dataGlobalReference.current = data
     const cartContext = useContext(CartContext)
     const isAdmin = useContext(UserRoleContext).isAdmin
-    const [snackBarMsg,setSnackbarMsg] = useState(undefined)
-    const addItem = (item,key) => {
-  
+    const [snackBarMsg, setSnackbarMsg] = useState(undefined)
+    const storageRefernce = storage()
+    const addItem = (item, key) => {
+
         const cartItem = cartContext.cart.find(itm => key == itm.key)
-        if(cartItem)
+        if (cartItem)
             cartItem.quantity = (cartItem.quantity || 0) + 1 //if for the first time then quantity incremented from 0 if 
-            //quantity undefined
+        //quantity undefined
         else
-            cartContext.cart.push({...item,cartQuantity : undefined,quantity : 1})
-        cartContext.setCart([...cartContext.cart])    
-    }   
-   
-    useEffect(()=>{
-        
-        reference.on('value',(snapshot)=>{
+            cartContext.cart.push({ ...item, cartQuantity: undefined, quantity: 1 })
+        cartContext.setCart([...cartContext.cart])
+    }
+
+    useEffect(() => {
+
+        reference.on('value', async (snapshot) => {
             const newData = []
             const dbData = snapshot.val()
-            for(const keyID in dbData){
-                newData.push({...dbData[keyID],key : keyID})
+            for (const keyID in dbData) {
+                newData.push({ ...dbData[keyID], key: keyID })
             }
+
             setData(newData)
         })
-    },[])
-    const requestDeleteConformation = (key,itemName) => {
+    }, [])
+    const requestDeleteConformation = (key, itemName) => {
         Alert.alert("Are you sure you want to delete ?",
-        `Do you want to delete ${itemName} permanantly from store`,
-        [
-            {
-                text : 'cancel',
-                style : 'cancel'
-            },
-            {
-                text : 'Yes Delete' , onPress : ()=>{
-                    reference.child(key).remove(()=>setSnackbarMsg('successfully removed food item'))
-                }, 
-                style :'destructive'
-            }
-        ],{ cancelable : true })
+            `Do you want to delete ${itemName} permanantly from store`,
+            [
+                {
+                    text: 'cancel',
+                    style: 'cancel'
+                },
+                {
+                    text: 'Yes Delete', onPress: async () => {
+                        await storageRefernce.ref('foodImages/' + key).delete()
+                        reference.child(key).remove(() => setSnackbarMsg('successfully removed food item'))
+
+                    },
+                    style: 'destructive'
+                }
+            ], { cancelable: true })
     }
     const updateItem = (item) => {
         //extract the details of the updating item....
-        const selectedItem = item
-        for(const key in selectedItem){
+        const selectedItem = { ...item }
+        for (const key in selectedItem) {
             //if the field is number convert to string
-            if(typeof selectedItem[key] == 'number')
+            if (typeof selectedItem[key] == 'number')
                 selectedItem[key] = selectedItem[key].toString()
         }
-        navigation.jumpTo('createFood',{ selectedItem : selectedItem, selectedCategories : item.categories })
+        navigation.jumpTo('createFood', { selectedItem: selectedItem, selectedCategories: item.categories })
     }
+    //onItemsVisibile function is requried not to remain constant and not re-declared in each re-render
+    //because onViewableItemsChanged prop in Flatlist specified the function argument should not be 
+    //changed on the fly...
+    const onItemsVisibile = useRef(async ({ viewableItems }) => {
+        for (const viewwableItem of viewableItems) {
+            if (typeof viewwableItem.item.image == 'string')
+                continue
+            viewwableItem.item.image = ''
+            storageRefernce.ref('foodImages/' + viewwableItem.item.key).getDownloadURL().then(url => {
+                viewwableItem.item.image = url
+                setData([...dataGlobalReference.current])
+            }, errorReason => {
+                // console.log(errorReason)
+            })
+        }
+    })
+
     const renderItem = (value) => {
         const item = value.item
         const cartItem = cartContext.cart.find(cartItem => cartItem.key == item.key)
         return <CustomCard>
-            <View>
-                <View style={{ alignSelf: 'flex-start' }}>
-                    <Text style={theme.foodTitle}>{item.name}</Text>
-                    <View style={theme.underline} />
-                </View>
-                <KeyValueText description="Description" value={item.description} isVerical />
-                <KeyValueText description='Price' value={item.price} />
-                <View style={styles.chipContainer}>
-                    {item['categories'].map(category=><Text key={category} style={styles.chip}>{category}</Text>)}
-                </View>
-                <CustomButton onPress={()=>{addItem(item,item.key)}} title="add To Cart" mode="contained" buttonStyle={{alignSelf :'flex-start'}} >
-                    {cartItem && <View style={styles.cartCountCaption}>
-                        <Text style={{color : theme.colors.primary}}>{cartItem.quantity}</Text>
+
+            <View style={{ flex: 1 }}>
+                {/* render image only if image url string is not empty */}
+                {((item.image || '') != '') && <Image source={{ uri: item.image }} style={styles.foodImage} />}
+                <View style={{flexDirection : 'row',justifyContent : 'space-between'}}>
+                    <View>
+                        <View style={{ alignSelf: 'flex-start' }}>
+                            <Text style={theme.foodTitle}>{item.name}</Text>
+                            <View style={theme.underline} />
+                        </View>
+
+                        <KeyValueText description="Description" value={item.description} isVerical />
+                        <KeyValueText description='Price' value={item.price} />
+                        {/* <KeyValueText description="Categories" value={item['categories'].join(',')} /> */}
+                        <View style={styles.chipContainer}>
+                            {item['categories'].map(category => <Text key={category} style={styles.chip}>{category}</Text>)}
+                        </View>
+                        <CustomButton onPress={() => { addItem(item, item.key) }} title="add To Cart" mode="contained" buttonStyle={{ alignSelf: 'flex-start' }} >
+                            {cartItem && <View style={styles.cartCountCaption}>
+                                <Text style={{ color: theme.colors.primary }}>{cartItem.quantity}</Text>
+                            </View>}
+                        </CustomButton>
+                    </View>
+                    {isAdmin && <View style={styles.itemIconContainer}>
+                        <Icon name="edit" size={25} color={theme.colors.primary} onPress={() => { updateItem(item) }} />
+                        <Icon style={styles.deleteButton} onPress={() => { requestDeleteConformation(item.key, item.name) }} name='trash-o' size={25} color={'red'} />
+
                     </View>}
-                </CustomButton>
+                </View>
             </View>
-            { isAdmin &&<View style={styles.itemIconContainer}>
-                <Icon name="edit" size={25} color={theme.colors.primary} onPress={()=>{updateItem(item)}}/>
-                <Icon style={styles.deleteButton} onPress={() => { requestDeleteConformation(item.key,item.name) }} name='trash-o' size={25} color={'red'} />
-                
-            </View>}
+
         </CustomCard>
     }
     //avoid re-rendering data if only snackBar message is updated...
-    const getSearchFilteredData = () => useMemo(()=>searchCriteria ? data.filter(val=>val.name.toLowerCase().includes(searchCriteria.toLowerCase())) : data,[searchCriteria,data])
-
+    const getSearchFilteredData = () => useMemo(() => searchCriteria ? data.filter(val => val.name.toLowerCase().includes(searchCriteria.toLowerCase())) : data, [searchCriteria, data])
     return <View style={styles.container}>
         <Text style={theme.headerStyle}>Browse Food</Text>
-        <CustomSearchBar placeholder='Search item' onSearch={setSearchCriteria}/>
-        <FlatList data={getSearchFilteredData()} renderItem={renderItem} />
-        <CustomSnackBar message={snackBarMsg} setMessage={setSnackbarMsg}/>
+        <CustomSearchBar placeholder='Search item' onSearch={setSearchCriteria} />
+        <FlatList data={getSearchFilteredData()} renderItem={renderItem}
+            viewabilityConfig={{
+                itemVisiblePercentThreshold: 50
+            }}
+            onViewableItemsChanged={onItemsVisibile.current} />
+        <CustomSnackBar message={snackBarMsg} setMessage={setSnackbarMsg} />
     </View>
 }
 const styles = StyleSheet.create({
@@ -109,36 +150,42 @@ const styles = StyleSheet.create({
     container: {
         height: '100%',
     },
-    cartCountCaption : {
-        alignItems : 'center',
-        marginLeft : 10,
-        padding : 5,
-        borderRadius : 100,
-        backgroundColor : 'white'
+    cartCountCaption: {
+        alignItems: 'center',
+        marginLeft: 10,
+        padding: 5,
+        borderRadius: 100,
+        backgroundColor: 'white'
     },
-    itemIconContainer : {
-        flexDirection : 'row',
-        alignItems : 'center'
+    itemIconContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
     },
-    chip : {
-        textAlign : 'center',
-        color : 'white',
-        width : 'auto',
-        backgroundColor : theme.colors.placeholder,
-        borderRadius : 10,
-        paddingHorizontal : 15,
-        padding : 5,
-        margin : 5
+    chip: {
+        textAlign: 'center',
+        color: 'white',
+        width: 'auto',
+        backgroundColor: theme.colors.secondary,
+        borderRadius: 10,
+        paddingHorizontal: 15,
+        padding: 5,
+        margin: 5
     },
-    chipContainer : {
-        flexDirection : 'row',
-        flexWrap : 'wrap'
+    chipContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
     },
-    deleteButton : {
+    foodImage: {
+        width : '100%',
+        height: 200,
+        marginRight: 10,
+        borderRadius: 5
+    },
+    deleteButton: {
         // height: 50,
         borderRadius: 50,
         backgroundColor: '#F5B5A8',
-        marginLeft : 10,
+        marginLeft: 10,
         alignItems: 'center',
         padding: 10,
         // aspectRatio: 1
--- components/ProductScreen.js
+++ components/ProductScreen.js
@@ -1,22 +1,23 @@
 import React, { useEffect, useState, useMemo } from 'react'
 import database from '@react-native-firebase/database';
-import { Modal, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Image, Modal, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import TextInput from './common/TextInput';
 
 import DropDownPicker from 'react-native-dropdown-picker';
 import theme from './common/theme';
 import CustomSnackBar from './common/CustomSnackBar';
 import CustomButton from './common/CustomButton';
-
+import storage from '@react-native-firebase/storage';
+import { launchImageLibrary } from 'react-native-image-picker';
 const fields = {
     name: {},
     description: { multiline: true },
     'discount limit': { validation: /^[0-9]+$/, numberOnly: true },
-    price: { validation: /^[0-9]+$/, numberOnly: true }
+    price: { validation: /^[0-9]+$/, numberOnly: true },
 }
-const refs = Object.keys(fields).map(_=> { return { ref : undefined } })
+const refs = Object.keys(fields).map(_ => { return { ref: undefined } })
 export default function ProductScreen({ route }) {
-    
+    const storageReference = storage()
     const reference = database().ref('/')
     const get = (key, fieldName = 'value') => fieldData[key] ? fieldData[key][fieldName] : undefined
     const [fieldData, setFieldData] = useState({})
@@ -24,36 +25,61 @@ export default function ProductScreen({ route }) {
     const [selectedCategories, setSelectedCategories] = useState([])
     const [dropdownOpen, setDropdownOpen] = useState(false)
     const [snackbarMsg, setSnackbarMsg] = useState(undefined)
-    useEffect(()=>{
-        
+    useEffect(() => {
+
         setFieldData({})
         // do not remove selected category list if selected item is already
         // provivded or else it will clear the category list of updating item
-        if(!(route.params?.selectedItem)){
+        if (!(route.params?.selectedItem)) {
             setSelectedCategories([])
-        }else{
+        } else {
             setSelectedCategories(route.params?.selectedCategories)
         }
-            
-    },[route.params?.selectedItem])
+
+    }, [route.params?.selectedItem])
 
     const changeInputText = (key, text) => {
         setFieldData({ ...fieldData, [key]: { 'value': text } })
     }
+    //for selecting image from gallery.....
+    const selectImage = async () => {
+        const results = await launchImageLibrary({ mediaType: 'photo' })
+        if (results.didCancel) {
+            return
+        }
+        //if user ever deprecate file access permission to the app....
+        if (results.errorCode == 'permission') {
+            setSnackbarMsg('Sorry permission is not granted to access files')
+            return
+        }
+        //like normal input field data the uri of the local image is saved in fieldData object...
+        setFieldData({ ...fieldData, 'image': { 'value': results.assets[0].uri } })
+    }
     const createOrUpdateFood = (updateItemKey) => {
-        if (validateFields(updateItemKey != undefined)){ //ignore checking empty fields if updating
+        if (validateFields(updateItemKey != undefined)) { //ignore checking empty fields if updating
             return
         }
         var parsedData = {}
+        const imagePath = fieldData.image?.value
         for (const key in fieldData) {
-           
-            parsedData[key] = fields[key].numberOnly ? parseFloat(fieldData[key].value) : fieldData[key].value
+            parsedData[key] = fields[key]?.numberOnly ? parseFloat(fieldData[key].value) : fieldData[key].value
         }
+        delete parsedData.image //safely delete 'image' attribute to prevent it from written into
+        //firebase database....
         parsedData.categories = selectedCategories //unwrapping the parameter
-        if (updateItemKey)
-            reference.child('food').child(updateItemKey).update(parsedData, () => { setSnackbarMsg('Product has successfully updated') })
-        else
-            reference.child('food').push(parsedData, () => { setSnackbarMsg('Product has successfully created') })
+        if (updateItemKey) { //update mode...
+            reference.child('food').child(updateItemKey).update(parsedData, async () => {
+                if (imagePath) // update image only if user have provided a one....
+                    await storageReference.ref('foodImages/' + updateItemKey).putFile(imagePath)
+                setSnackbarMsg('Product has successfully updated')
+            })
+        }
+        else { // create mode...
+            let newRef = reference.child('food').push(parsedData, async () => {
+                await storageReference.ref('foodImages/' + newRef.key).putFile(imagePath)
+                setSnackbarMsg('Product has successfully created')
+            })
+        }
 
     }
     const validateFields = (ignoreEmptyFields = false) => {
@@ -80,6 +106,14 @@ export default function ProductScreen({ route }) {
                     validationFailed = true
                 }
             }
+        }
+        
+        if (!ignoreEmptyFields && !get('image')) {
+            setSnackbarMsg('Should select a product image')
+            //instantly failed the validation and no need to
+            //update errorFields when user not provided a image only on create mode - when
+            //ignoreEmptyFields is false
+            return true
         }
         if (selectedCategories.length == 0) {
             setSnackbarMsg('Food should at least belong to single category')
@@ -112,29 +146,30 @@ export default function ProductScreen({ route }) {
         })
 
     }, [])
-    const parsedCategoryListForDropDown =  useMemo(()=>categories.map(category => { return { value: category, label: category } }),[categories])
+    const parsedCategoryListForDropDown = useMemo(() => categories.map(category => { return { value: category, label: category } }), [categories])
     const generateUIFields = () => {
-        const uiFields = 
+        var uiFields =
+            //pop() called to remove last 'image' field as it is not text input field...
             Object.keys(fields).map((field, index) => <TextInput
                 key={index}
                 description={field}
                 label={field}
                 keyboardType={fields[field].numberOnly ? "decimal-pad" : "default"}
-                innerRef={(input)=>refs[index].ref = input}
-                onSubmitEditing={()=>{
-                if(index < refs.length - 1)  
-                    refs[index + 1].ref.focus()
+                innerRef={(input) => refs[index].ref = input}
+                onSubmitEditing={() => {
+                    if (index < refs.length - 1)
+                        refs[index + 1].ref.focus()
                 }}
-                defaultValue={ route.params?.selectedItem ? route.params.selectedItem[field] : undefined }
+                defaultValue={route.params?.selectedItem ? route.params.selectedItem[field] : undefined}
                 returnKeyType={index == (fields.length - 1) ? 'done' : 'next'}
                 onChangeText={(text) => { changeInputText(field, text) }}
                 error={get(field, 'error')}
                 multiline={fields[field].multiline}
                 errorText={get(field, 'error') || ''}
             />)
-          // add the category dropdown in in 3rd position in input fields...  
-         uiFields.splice(2, 0, <View key="categoryDropdown"><Text style={{ marginBottom: 10 }}>Category</Text><SafeAreaView key="dropDown" style={{ flex: 1 }}>
-            
+        // add the category dropdown in in 3rd position in input fields...  
+        uiFields.splice(2, 0, <View key="categoryDropdown"><Text style={{ marginBottom: 10 }}>Category</Text><SafeAreaView key="dropDown" style={{ flex: 1 }}>
+
             <DropDownPicker
                 listMode="SCROLLVIEW"
                 open={dropdownOpen}
@@ -152,7 +187,16 @@ export default function ProductScreen({ route }) {
                 items={parsedCategoryListForDropDown}
             />
         </SafeAreaView></View>)
-         
+        //add image field.....
+        //if either field data or route params have image use it for source props....
+        //if user haven't picked a image then fieldData['image']?.value would be undefined and
+        //original image of the product is should be shown given by route.params (if the product is updating)
+        uiFields.push(
+            useMemo(() => route.params?.selectedItem.image || fieldData['image']?.value ?
+                <Image key="uploadImg" source={{ uri: fieldData['image']?.value || route.params?.selectedItem.image }}
+                    style={{ width: 200, height: 200 }} /> : null, [fieldData['image']?.value, route.params?.selectedItem.image]
+            )
+        )
         return uiFields
     }
 
@@ -160,6 +204,7 @@ export default function ProductScreen({ route }) {
         <ScrollView>
             <Text style={styles.header}>Add Food</Text>
             {generateUIFields()}
+            <CustomButton title="Select a Image" mode="outlined" onPress={selectImage} buttonStyle={{ marginVertical: 10 }} />
             {/* function will create or update depends if a existed item is provided in route param... */}
             <CustomButton mode="contained" onPress={() => { createOrUpdateFood(route.params?.selectedItem?.key) }} title={route.params?.selectedItem ? 'Update' : 'Create'} />
         </ScrollView>
